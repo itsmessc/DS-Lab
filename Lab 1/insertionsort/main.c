a33340cda368c1a9d67befa6601fe810091235f2
Sathwik004 <113238935+Sathwik004@users.noreply.github.com>
2023-09-10 14:17:28 +0530

    user defined func

--- Lab 1/insertionsort/main.c
+++ Lab 1/insertionsort/main.c
@@ -1,17 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-int main()
+void insertionSort(int *a,int n)
 {
-    int n;
-    printf("Enter the size of the array:");
-    scanf("%d",&n);
-    int a[n],i,j,temp;
-    printf("Enter elements:");
-    for(i=0;i<n;i++)
-    {
-       scanf("%d",&a[i]);
-    }
+    int i,j,temp;
     for(i=1;i<n;i++)
     {
         temp=a[i];
@@ -24,6 +15,19 @@ int main()
         }
         a[j+1]=temp;
     }
+}
+int main()
+{
+    int n;
+    printf("Enter the size of the array:");
+    scanf("%d",&n);
+    int a[n],i,j,temp;
+    printf("Enter elements:");
+    for(i=0;i<n;i++)
+    {
+       scanf("%d",&a[i]);
+    }
+    insertionSort(a,n);
     printf("Array after sorting:");
     for(i=0;i<n;i++)
     {
