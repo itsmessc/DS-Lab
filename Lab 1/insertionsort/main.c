430786901c1effa0b7bb1d07b14101306bd53c8e
Sathwik004 <113238935+Sathwik004@users.noreply.github.com>
2023-09-10 14:22:48 +0530

    Update main.c

--- Lab 1/insertionsort/main.c
+++ Lab 1/insertionsort/main.c
@@ -21,7 +21,7 @@ int main()
     int n;
     printf("Enter the size of the array:");
     scanf("%d",&n);
-    int a[n],i,j,temp;
+    int a[n],i;
     printf("Enter elements:");
     for(i=0;i<n;i++)
     {
